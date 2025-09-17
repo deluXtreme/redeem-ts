30f66c81833336061779c24fb4a14816d3567227
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-09-17 15:16:44 +0200

    Set With Wrap to False

--- actions/src/utils.ts
+++ actions/src/utils.ts
@@ -49,7 +49,7 @@ export async function getFlowMatrix(
     from,
     to,
     targetFlow,
-    useWrappedBalances: true,
+    useWrappedBalances: false,
   });
   return createFlowMatrix(from, to, targetFlow, path.transfers);
 }
--- actions/test/encode.spec.ts
+++ actions/test/encode.spec.ts
@@ -75,4 +75,37 @@ describe("encodeCallData", () => {
       sourceCoordinate,
     }).toEqual(flowMatrix);
   });
+
+  it("e2e: test on subscription", async () => {
+    const sampleData = {
+    "id": "0x9bafd7444ba424830038c3357cd0df67361781a317e5d12315e6a07d9ebd03d2",
+    "subscriber": "0xcf6dc192dc292d5f2789da2db02d6dd4f41f4214",
+    "recipient": "0x6b69683c8897e3d18e74b1ba117b49f80423da5d",
+    "amount": "10000000000000000",
+    "periods": 157,
+    "category": "trusted",
+    "next_redeem_at": 1757540225
+  } as RedeemableSubscription;
+    const flowMatrix = await getFlowMatrix(
+      "https://rpc.aboutcircles.com/",
+      sampleData,
+    );
+    console.log("Flow Matrix", flowMatrix);
+    const encoded = encodeCallData(flowMatrix);
+    console.log("Encoded", encoded)
+    const [
+      flowVertices,
+      flowEdges,
+      streams,
+      packedCoordinates,
+      sourceCoordinate,
+    ] = decodeAbiParameters(FlowMatrixAbi, encoded);
+    expect({
+      flowVertices,
+      flowEdges,
+      streams,
+      packedCoordinates,
+      sourceCoordinate,
+    }).toEqual(flowMatrix);
+  });
 });
